82f7ae97b88b62f1702e104767898e33d5a5c7aa
Mark van Holsteijn <mark.van.holsteijn@gmail.com>
2020-06-20 16:23:46 +0200

    add skeleton for the list command

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/binxio/cru
+
+go 1.14
+
+require (
+	github.com/docopt/docopt-go v0.0.0-20180111231733-ee0de3bc6815
+	golang.org/x/tools v0.0.0-20200619210111-0f592d2728bb
+)
---
+++ main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"fmt"
+	"github.com/docopt/docopt-go"
+	"golang.org/x/tools/godoc/util"
+	"golang.org/x/tools/godoc/vfs"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+)
+
+func list_references(filename string) error {
+	content, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return fmt.Errorf("could not read %s, %s", filename, err)
+	}
+	log.Printf("reading %d bytes from %s\n", len(content), filename)
+	return nil
+}
+
+func list(paths []string) error {
+	for _, path := range paths {
+		info, err := os.Stat(path)
+		if err != nil {
+			return fmt.Errorf("%s is not a valid path or is not readable, %s", path, err)
+		}
+		if info.IsDir() {
+			err := filepath.Walk(path,
+				func(p string, info os.FileInfo, err error) error {
+					if err != nil {
+						return err
+					}
+					if util.IsTextFile(vfs.OS(filepath.Dir(p)), filepath.Base(p)) {
+						list_references(p)
+					}
+					return nil
+				})
+			if err != nil {
+				return err
+			}
+		} else {
+			log.Println(path)
+			if !util.IsTextFile(vfs.OS(filepath.Dir(path)), filepath.Base(path)) {
+				return fmt.Errorf("%s is not a text file", path)
+			}
+			list_references(path)
+		}
+	}
+	return nil
+}
+
+func main() {
+	usage := `cru - container image reference updater
+
+Usage:
+  cru list [PATH] ...
+  cru -h | --help
+
+Options:
+  list - image references in the specified files and directories
+  -h --help     Show this screen.
+`
+	args, _ := docopt.ParseDoc(usage)
+	if _, err := args.Bool("list"); err == nil {
+		paths := args["PATH"].([]string)
+		if len(paths) == 0 {
+			paths = append(paths, ".")
+		}
+		list(paths)
+	}
+}
