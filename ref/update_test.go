3623b886088b49e55c2d72356d0d029e0c5f99da
cru <cru@binx.io>
2020-07-25 15:32:35 +0200

    paas-monitor to latest

--- ref/update_test.go
+++ ref/update_test.go
@@ -20,15 +20,15 @@ func TestUpdateReferenceSimple(t *testing.T) {
 
 func TestUpdateReferenceMultiple(t *testing.T) {
 	input := []byte(`this is one gcr.io/binx-io-public/paas-monitor:v0.3.1 reference
-and this is another mvanholsteijn/paas-monitor:v0.1.0
+and this is another mvanholsteijn/paas-monitor:latest
 and this is just a directory Name mvanholsteijn/paas-monitor, which should
 not be changed`)
 	expect := []byte(`this is one gcr.io/binx-io-public/paas-monitor:v1.0.0 reference
-and this is another mvanholsteijn/paas-monitor:v0.2.0-beta
+and this is another mvanholsteijn/paas-monitor:latest
 and this is just a directory Name mvanholsteijn/paas-monitor, which should
 not be changed`)
 	references := []ContainerImageReference{*MustNewContainerImageReference(`gcr.io/binx-io-public/paas-monitor:v1.0.0`),
-		*MustNewContainerImageReference(`mvanholsteijn/paas-monitor:v0.2.0-beta`)}
+		*MustNewContainerImageReference(`mvanholsteijn/paas-monitor:latest`)}
 
 	result, updated := UpdateReferences(input, references)
 	if !updated {
